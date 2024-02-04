d7fd818b5ef6a5d338ffcb66dfc1ca28670340d1
M-Saffan75 <saffan.khan175@gmail.com>
2024-02-04 12:15:56 -0700

    syntax correct

--- App.js
+++ App.js
@@ -1,44 +1,26 @@
 
 import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import OnboardingScreen from './src/screens/OnboardingScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
 
-  // React.useEffect(() => {
-  //   const checkAppFirstLaunch = async () => {
-  //     const appData = await AsyncStorage.getItem('isAppFirstLaunched');
-  //     if (appData === null) {
-  //       setIsAppFirstLaunched(true);
-  //       await AsyncStorage.setItem('isAppFirstLaunched', 'false');
-  //     } else {
-  //       setIsAppFirstLaunched(false);
-  //     }
-  //   };
-  //   checkAppFirstLaunch();
-  // }, []);
-
+  
   return (
-    // isAppFirstLaunched != null && (
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {/* {isAppFirstLaunched && ( */}
             <Stack.Screen
               name="OnboardingScreen"
               component={OnboardingScreen}
             />
-          {/* )} */}
           <Stack.Screen name="HomeScreen" component={HomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     )
-  // );
 };
 
 export default App;
